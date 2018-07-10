6bf36cc5fa923bae247fbc863216bf69925b88bf
Tushar Adhao <tuswork0@gmail.com>
2018-07-10 16:37:20 +0530

    rearrange order of content in applicationJs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require toastr
+
 //= require jquery
-//= require bootstrap-sprockets 
-//= require rails-ujs
+//= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
+//= require toastr
 //= require_tree .
